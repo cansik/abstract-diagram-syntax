bf020e11ce80f49ad4cbed1c9c86a7f14c2e627d
Florian Bruggisser <florian@bildspur.ch>
2018-08-16 19:14:45 +0200

    implemented assignment parsing

--- src/main/kotlin/ch/bildspur/ads/syntaxtree/AssignmentElement.kt
+++ src/main/kotlin/ch/bildspur/ads/syntaxtree/AssignmentElement.kt
@@ -3,4 +3,8 @@ package ch.bildspur.ads.syntaxtree
 class AssignmentElement(val target : String,
                         val name : String,
                         val value : String) : SyntaxElement() {
+
+    override fun toString(): String {
+        return "AssignmentElement(target='$target', name='$name', value='$value')"
+    }
 }
--- src/main/kotlin/ch/bildspur/ads/syntaxtree/SyntaxTreeParser.kt
+++ src/main/kotlin/ch/bildspur/ads/syntaxtree/SyntaxTreeParser.kt
@@ -8,7 +8,7 @@ class SyntaxTreeParser {
 
     val commentRegex = Regex("^\\s*#(.*)\$")
     val assignmentRegex = Regex("^\\s*(\\w+)\\.(\\w+)\\s*=\\s*\"([\\w\\s]*)\"")
-    val methodRegex = Regex("^\\s*(\\w+)\\.(\\w+)\\(\"([\\w\\s]*)\"\\)")
+    val actionRegex = Regex("^(\\s*)(\\w+)\\.(\\w+)\\(\"?([\\w\\s]*)\"?\\)")
 
     data class ElementDetectionResult(val type : ElementType, val result : MatchResult?)
 
@@ -28,18 +28,18 @@ class SyntaxTreeParser {
 
         // detect intent and merge if possible
         val intent = intentRegex.findAll(line).map { it.value.length }.sum()
+        /*
         if(tree.currentIntent != intent)
             tree.merge()
+        */
 
         // parse elements
+        val values = parsedLine.result.groupValues
         when(parsedLine.type)
         {
-            ElementType.COMMENT -> {
-                val comment = CommentElement(parsedLine.result.groupValues[0])
-                tree.add(comment)
-            }
+            ElementType.COMMENT -> tree.add(CommentElement(values[1]))
             ElementType.ACTION -> TODO()
-            ElementType.ASSIGNMENT -> TODO()
+            ElementType.ASSIGNMENT -> tree.add(AssignmentElement(values[1], values[2], values[3]))
             ElementType.BRANCH -> TODO()
             ElementType.NONE -> {}
         }
@@ -48,9 +48,17 @@ class SyntaxTreeParser {
     private fun detectElementType(line : String) : ElementDetectionResult
     {
         lateinit var result : Sequence<MatchResult>
+
+        // comment
         if({result = commentRegex.findAll(line); result}().count() > 0)
         {
             return ElementDetectionResult(ElementType.COMMENT, result.first())
+        }
+
+        // assignment
+        if({result = assignmentRegex.findAll(line); result}().count() > 0)
+        {
+            return ElementDetectionResult(ElementType.ASSIGNMENT, result.first())
         }
 
         return ElementDetectionResult(ElementType.NONE, null)
